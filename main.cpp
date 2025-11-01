f39d89038d83ab489c81573d2c1fe82569abddd4
Petrov Vadim <vadikdydy@mail.ru>
2025-11-01 12:40:28 +0300

    make create() function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
+#include <cstddef>
+#include <iostream>
+
+
+
+int ** create(size_t rows, size_t cols)
+{
+  int ** mtx = new int * [rows];
+  size_t created = 0;
+  try
+  {
+    for (; created < rows; ++created)
+    {
+      mtx[created] = new int[cols];
+    }  
+  }
+  catch(const std::bad_alloc & e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
+  return mtx;
+}
+
 int main()
 {
-  return 0;
+  
 }
